d1eaf81ad6f446bd9da2c3ee7124b93aff47b8fb
Tisha Mazumdar <114134745+TishaMazumdar@users.noreply.github.com>
2023-10-21 18:14:23 +0530

    Including No of Attempts

--- RandomNumGuess.java
+++ RandomNumGuess.java
@@ -1,11 +1,10 @@
-package MidSemPracticeIII;
 import java.util.Scanner;
 
 public class RandomNumGuess {
     public static void main(String args[]){
         int myNum = (int)(Math.random()*100);
         Scanner sc = new Scanner(System.in);
-        int num;
+        int num, count = 1;
         
         System.out.println("GUESS A NUMBER FROM 0 TO 100. ENTER -1 TO EXIT");
         do{
@@ -14,16 +13,19 @@ public class RandomNumGuess {
             if(num < -1){
                 System.out.println("Enter -1 to exit.");
             } else if(num == -1){
-                System.out.println("You quit... The number was "+myNum);
+                System.out.println("You quit... The number was " + myNum);
             } else if (num > 100){
                 System.out.println("The number is between 0 to 100.");
             } else {
                 if(num > myNum){
                     System.out.println("Too high, try again!");
+                    count += 1;
                 } else if (num < myNum){
                     System.out.println("Too low, try again!");
+                    count += 1;
                 } else {
                     System.out.println("Well done! You win!");
+                    System.out.println("Number of attempts = " + count);
                     break;
                 }
             }
